22e1269caf540006571389cf61a58a68a56ab853
ruslannovik <novik.rv93@gmail.com>
2021-04-02 13:07:17 +0300

    fix: add label for toggle

--- online-zoo/pages/landing/landing.js
+++ online-zoo/pages/landing/landing.js
@@ -1,5 +1,6 @@
 const bannerSliderList = document.querySelector('.banner-slider__list');
 const bannerSliderImgs = document.querySelectorAll('.banner-slider__img');
+const bannerSliderContainer = document.querySelectorAll('.banner-slider__container');
 
 bannerSliderList.addEventListener('mousemove', handlerBannerSliderClick);
 
@@ -7,6 +8,8 @@ bannerSliderList.addEventListener('mousemove', handlerBannerSliderClick);
 function handlerBannerSliderClick({ target }) {
   if (target.classList.contains('banner-slider__img')) {
     bannerSliderImgs.forEach(item => item.classList.remove('banner-slider__img_active'));
+    bannerSliderContainer.forEach(item => item.classList.remove('banner-slider__container_active'));
     target.classList.toggle('banner-slider__img_active');
+    target.parentNode.classList.toggle('banner-slider__container_active');
   }
 }
