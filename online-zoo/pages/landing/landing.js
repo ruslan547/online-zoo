23e205be4d3e94c4e881b5ece6c2cf81885cb58a
ruslannovik <novik.rv93@gmail.com>
2021-04-08 17:11:07 +0300

    feat: add min for landing

--- online-zoo/pages/landing/landing.js
+++ online-zoo/pages/landing/landing.js
@@ -1,6 +1,6 @@
 const bannerSliderList = document.querySelector('.banner-slider__list');
 const bannerSliderImgs = document.querySelectorAll('.banner-slider__img');
-const bannerSliderContainer = document.querySelectorAll('.banner-slider__container');
+const bannerSliderItem = document.querySelectorAll('.banner-slider__item');
 
 bannerSliderList.addEventListener('mousemove', handlerBannerSliderClick);
 
@@ -8,8 +8,8 @@ bannerSliderList.addEventListener('mousemove', handlerBannerSliderClick);
 function handlerBannerSliderClick({ target }) {
   if (target.classList.contains('banner-slider__img')) {
     bannerSliderImgs.forEach(item => item.classList.remove('banner-slider__img_active'));
-    bannerSliderContainer.forEach(item => item.classList.remove('banner-slider__container_active'));
+    bannerSliderItem.forEach(item => item.classList.remove('banner-slider__item_active'));
     target.classList.toggle('banner-slider__img_active');
-    target.parentNode.classList.toggle('banner-slider__container_active');
+    target.parentNode.parentNode.classList.toggle('banner-slider__item_active');
   }
 }
