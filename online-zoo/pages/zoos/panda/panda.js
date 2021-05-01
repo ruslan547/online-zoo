4a6cd6b97cb1aeb8ff7ba77f7683eb159c3a64fd
ruslannovik <novik.rv93@gmail.com>
2021-05-01 13:25:30 +0300

    feat: connect youtobe player

--- online-zoo/pages/zoos/panda/panda.js
+++ online-zoo/pages/zoos/panda/panda.js
@@ -1,9 +1,19 @@
+//Theme
 const themeToggle = document.querySelector('.theme-toggle');
+//iframes
+const iframes = document.querySelector('.iframes');
+const ytApiScript = document.querySelector('script');
+let player;
 
+//Theme
 themeToggle.addEventListener('click', handleThemeToggleClick);
+//iframes
+iframes.addEventListener('click', handleIframesClick);
+ytApiScript.addEventListener('load', handleScriptLoad);
 
 initTheme();
 
+//Theme
 function handleThemeToggleClick({ target }) {
   if (target.id === 'toggle') {
     return;
@@ -22,3 +32,52 @@ function initTheme() {
   document.documentElement.setAttribute('theme', theme);
   toggle.checked = theme === 'durk';
 }
+
+//iframe
+
+
+function handleIframesClick({ target }) {
+  const iframesItem = target.parentNode;
+  const iframe = document.querySelector('.zoo__iframe');
+
+
+
+}
+
+////////////////////////
+
+function handleScriptLoad() {
+  video.onload = () => {
+    onYouTubeIframeAPIReady();
+  }
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('video', {
+    events: {
+      'onReady': onPlayerReady,
+      // 'onStateChange': onPlayerStateChange,
+    },
+    playerVars: {
+      'origin': 'http://localhost:8080'
+    },
+  });
+}
+
+function onPlayerReady({ target }) {
+  //target.playVideo();
+
+  // btn.addEventListener('click', () => {
+  //   target.pauseVideo()
+  // });
+}
+
+let done = false;
+function onPlayerStateChange(event) {
+  console.log('change')
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
