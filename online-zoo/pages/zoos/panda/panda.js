321f7caffabf142347eca8b7c5b84837e1837b61
ruslannovik <novik.rv93@gmail.com>
2021-05-01 16:51:54 +0300

    feat: add zoo slider

--- online-zoo/pages/zoos/panda/panda.js
+++ online-zoo/pages/zoos/panda/panda.js
@@ -2,14 +2,13 @@
 const themeToggle = document.querySelector('.theme-toggle');
 //iframes
 const iframes = document.querySelector('.iframes');
-const ytApiScript = document.querySelector('script');
-let player;
+const iframeArr = Array.from(iframes.children);
+const zooScreen = document.querySelector('.zoo__screen');
 
 //Theme
 themeToggle.addEventListener('click', handleThemeToggleClick);
 //iframes
 iframes.addEventListener('click', handleIframesClick);
-ytApiScript.addEventListener('load', handleScriptLoad);
 
 initTheme();
 
@@ -34,50 +33,28 @@ function initTheme() {
 }
 
 //iframe
-
-
 function handleIframesClick({ target }) {
   const iframesItem = target.parentNode;
   const iframe = document.querySelector('.zoo__iframe');
+  const itemCopy = iframesItem.lastElementChild.cloneNode();
 
+  zooScreen.classList.add('zoo__screen_active');
+  itemCopy.width = getComputedStyle(iframe).width;
+  itemCopy.height = getComputedStyle(iframe).height;
+  itemCopy.src = `${itemCopy.src}?autoplay=1`;
+  itemCopy.classList.add('zoo__iframe');
+  itemCopy.id = iframe.id;
 
+  iframe.width = getComputedStyle(iframesItem.lastElementChild).width;
+  iframe.height = getComputedStyle(iframesItem.lastElementChild).height;
+  iframe.src = iframe.src.replace('?autoplay=1', '');
+  iframe.classList.remove('zoo__iframe');
+  iframe.id = iframesItem.lastElementChild.id;
 
-}
+  iframesItem.replaceChild(iframe, iframesItem.lastElementChild);
+  iframes.before(itemCopy);
 
-////////////////////////
-
-function handleScriptLoad() {
-  video.onload = () => {
-    onYouTubeIframeAPIReady();
+  itemCopy.onload = () => {
+    zooScreen.classList.remove('zoo__screen_active');
   }
 }
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('video', {
-    events: {
-      'onReady': onPlayerReady,
-      // 'onStateChange': onPlayerStateChange,
-    },
-    playerVars: {
-      'origin': 'http://localhost:8080'
-    },
-  });
-}
-
-function onPlayerReady({ target }) {
-  //target.playVideo();
-
-  // btn.addEventListener('click', () => {
-  //   target.pauseVideo()
-  // });
-}
-
-let done = false;
-function onPlayerStateChange(event) {
-  console.log('change')
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-
