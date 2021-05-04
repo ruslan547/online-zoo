eabadbc5c62ec3f0ee29e7faeb5033ec13d26baf
ruslannovik <novik.rv93@gmail.com>
2021-05-04 09:44:29 +0300

    feat: add slider for all zoos

--- online-zoo/pages/zoos/eagle/eagle.js
+++ online-zoo/pages/zoos/eagle/eagle.js
@@ -1,9 +1,18 @@
+//Theme
 const themeToggle = document.querySelector('.theme-toggle');
+//iframes
+const iframes = document.querySelector('.iframes');
+const iframeArr = Array.from(iframes.children);
+const zooScreen = document.querySelector('.zoo__screen');
 
+//Theme
 themeToggle.addEventListener('click', handleThemeToggleClick);
+//iframes
+iframes.addEventListener('click', handleIframesClick);
 
 initTheme();
 
+//Theme
 function handleThemeToggleClick({ target }) {
   if (target.id === 'toggle') {
     return;
@@ -21,4 +30,29 @@ function initTheme() {
 
   document.documentElement.setAttribute('theme', theme);
   toggle.checked = theme === 'durk';
+}
+
+//iframe
+function handleIframesClick({ target }) {
+  const iframesItem = target.parentNode;
+  const iframe = document.querySelector('.zoo__iframe');
+  const itemCopy = iframesItem.lastElementChild.cloneNode();
+
+  zooScreen.classList.add('zoo__screen_active');
+  itemCopy.width = getComputedStyle(iframe).width;
+  itemCopy.height = getComputedStyle(iframe).height;
+  itemCopy.src = `${itemCopy.src}?autoplay=1`;
+  itemCopy.classList.add('zoo__iframe');
+
+  iframe.width = getComputedStyle(iframesItem.lastElementChild).width;
+  iframe.height = getComputedStyle(iframesItem.lastElementChild).height;
+  iframe.src = iframe.src.replace('?autoplay=1', '');
+  iframe.classList.remove('zoo__iframe');
+
+  iframesItem.replaceChild(iframe, iframesItem.lastElementChild);
+  iframes.before(itemCopy);
+
+  itemCopy.onload = () => {
+    zooScreen.classList.remove('zoo__screen_active');
+  }
 }
